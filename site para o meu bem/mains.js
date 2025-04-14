0a409433b717a4178618e4eaa84bb92a30a81c64
CelinaBenedito <celina.benedito@sptech.school>
2025-04-14 03:05:22 +0000

    Adição de saldo

--- site para o meu bem/mains.js
+++ site para o meu bem/mains.js
@@ -1,4 +1,4 @@
-async function registrar(){
+function registrar(){
     var data = ipt_data.value;
     var valor = Number(ipt_valor.value);
     var titulo = ipt_nome.value;
@@ -8,7 +8,44 @@ async function registrar(){
     var valida = 0;
 
     for(var i=0; valida<3; i++){
-
+        if(titulo !=" "){
+            valida++
+        }
+        else if(valor > 0){
+            valida++
+        }
+        else if(data != " " || data != 0){
+            valida++
+        }
+        if(data == " " || data == 0){
+            alert("Data inválida");
+        }
+        else if(valor <= 0){
+            alert("Valor inválido");
+        }
+        if(titulo !=" "){
+           alert("Valor inválido")
+        }
     }
 }
-    
+// tela de ver registros
+let saldo = 0;
+if(valor>0){
+    saldo - valor;
+}
+
+function adicionarValor(){
+
+    div_card.innerHTML = `
+        <div class="card">
+            <p>Adicione o valor ao saldo</p>
+            <input type="number" placeholder="Ex: 10.00" id="ipt_valor">
+            <div class="buttons">
+            <button onclick="salvar()">Salvar</button>
+            </div>
+        </div>`
+}
+function salvar(){
+    let valorAdicionado = Number(ipt_valor.value);
+    saldo = saldo + valorAdicionado; 
+}
