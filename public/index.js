eddc9935979abfbc3e64b52ea57246bea43750ec
Celina Benedito <celina.benedito@sptech.school>
2025-12-09 13:57:29 -0300

    Add sidebar toggle functionality in index.js
    Implement sidebar functionality to toggle display of elements.

---
+++ public/index.js
@@ -0,0 +1,30 @@
+
+document.getElementById("containerGeral").style.gridTemplateColumns = "5% 90%"
+home.style.display = "none"
+reg.style.display = "none"
+add.style.display = "none"
+agenda.style.display = "none"
+
+function sidebarFunction() {
+    c = 0;
+    if (c == 1) {
+        document.getElementById("containerGeral").style.gridTemplateColumns = "5% 90%"
+        home.style.display = "none"
+        reg.style.display = "none"
+        add.style.display = "none"
+        agenda.style.display = "none"
+        c-1
+        console.log("if 1",c)
+    }
+    if (c == 0) {
+        document.getElementById("containerGeral").style.gridTemplateColumns = "15% 80%"
+        home.style.display = ""
+        reg.style.display = ""
+        add.style.display = ""
+        agenda.style.display = ""
+        c++
+        console.log("if 0",c)
+    }
+
+
+}
