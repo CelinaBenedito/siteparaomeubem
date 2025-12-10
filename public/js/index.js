57218a321bdee7f849d8599e45f78f4830e0dff1
CelinaBenedito <celina.benedito@sptech.school>
2025-12-10 13:49:10 -0300

    Finalizada estrutura base nova do site

--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,126 @@
+const navbar = document.getElementById("navbar");
+const main = document.getElementById("main");
+const home = document.getElementById("home");
+const reg = document.getElementById("reg");
+const add = document.getElementById("add");
+const agenda = document.getElementById("agenda");
 
-document.getElementById("navbar").style.width = "70px"
-home.style.display = "none"
-reg.style.display = "none"
-add.style.display = "none"
-agenda.style.display = "none"
+let ativo = false;
+
+navbar.style.width = "70px";
+home.style.display = "none";
+reg.style.display = "none";
+add.style.display = "none";
+agenda.style.display = "none";
+main.style.marginLeft = "70px";
 
 function sidebarFunction() {
-    c = 0;
-    if (c == 1) {
-        document.getElementById("navbar").style.gridTemplateColumns = "5% 90%"
-        document.getElementById("main").style.marginLeft = "50px";
+    console.log("Entrei na funciton", ativo)
+    if (!ativo) {
 
-        home.style.display = "none"
-        reg.style.display = "none"
-        add.style.display = "none"
-        agenda.style.display = "none"
-        c-1
-        console.log("if 1",c)
-    }
-    if (c == 0) {
-        document.getElementById("containerGeral").style.gridTemplateColumns = "15% 80%"
-        home.style.display = ""
-        reg.style.display = ""
-        add.style.display = ""
-        agenda.style.display = ""
-        c++
-        console.log("if 0",c)
+        ativo = true;
+
+        navbar.style.width = "290px";
+        main.style.marginLeft = "290px";
+        home.style.display = "";
+        reg.style.display = "";
+        add.style.display = "";
+        agenda.style.display = "";
+
+        console.log("Abriu", ativo);
+
+    } else {
+        ativo = false;
+
+        navbar.style.width = "70px";
+        main.style.marginLeft = "70px";
+        home.style.display = "none";
+        reg.style.display = "none";
+        add.style.display = "none";
+        agenda.style.display = "none";
+
+        console.log("Fechou", ativo);
     }
 
 
 }
+
+function gerarKPIS() {
+    for (c = 0; c < 1; c++) {
+        containerKPIS.innerHTML +=
+            `
+                <div class="KPI">
+                   <p>Total: <span>Nubank</span></p> 
+                   <p><span>R$1,000</span></p> 
+                </div> 
+            `
+    }
+
+}
+
+var options = {
+  chart: {
+    height:350,
+    type: 'bar'
+  },
+  series: [{
+    name: 'sales',
+    data: [30,40,35,50,49,60,70,91,125]
+  }],
+  xaxis: {
+    categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+  }
+}
+
+var tipos = new ApexCharts(document.querySelector("#graficoTipo"), options);
+
+tipos.render();
+
+var opcoes = {
+  chart: {
+    height:350,
+    type: 'line'
+  },
+  series: [{
+    name: 'sales',
+    data: [30,40,35,50,49,60,70,91,125]
+  }],
+  xaxis: {
+    categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+  }
+}
+
+var temporal = new ApexCharts(document.querySelector("#graficoTemporal"), opcoes);
+
+temporal.render();
+
+ opcaoDonut = {
+    chart:{
+        type: 'donut'
+    },
+    series: [50,30],
+    labels: ['Apple', 'Mango'],
+    fill:{
+        colors: ['#B4D9D5', '#367373']
+    }, 
+    plotOptions: {
+      pie: {
+        customScale: 0.8
+      }
+    },
+    responsive: [
+        {
+            breakpoint: 480,
+            options: {
+                chart: {
+                    width: '100%'
+                }
+            }
+        }
+    ]
+  }
+
+
+  var Donut = new ApexCharts(document.querySelector("#graficoDonut"), opcaoDonut)
+
+  Donut.render();
