ce61956337329b013ec9dc971e12a7d788677972
CelinaBenedito <celina.benedito@sptech.school>
2025-12-27 22:55:09 -0300

    Adição do dark theme

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -5,6 +5,7 @@ const reg = document.getElementById("reg");
 const add = document.getElementById("add");
 const agenda = document.getElementById("agenda");
 const config = document.getElementById("config");
+const tema = document.getElementById("tema");
 
 let ativo = false;
 
@@ -13,13 +14,14 @@ home.style.display = "none";
 reg.style.display = "none";
 add.style.display = "none";
 agenda.style.display = "none";
-config.style.display = "none"
+config.style.display = "none";
+tema.style.display = "none";
 main.style.marginLeft = "70px";
+
 
 function sidebarFunction() {
     console.log("Entrei na funciton", ativo)
     if (!ativo) {
-
         ativo = true;
 
         navbar.style.width = "290px";
@@ -28,8 +30,8 @@ function sidebarFunction() {
         reg.style.display = "";
         add.style.display = "";
         agenda.style.display = "";
-        config.style.display = ""
-
+        config.style.display = "";
+        tema.style.display = "";
 
         console.log("Abriu", ativo);
 
@@ -42,11 +44,38 @@ function sidebarFunction() {
         reg.style.display = "none";
         add.style.display = "none";
         agenda.style.display = "none";
-        config.style.display = "none"
-
+        config.style.display = "none";
+        tema.style.display = "none";
 
         console.log("Fechou", ativo);
     }
 
-
 }
+
+/*---------------- Tema dark ----------------*/
+const theme = localStorage.getItem("theme");
+
+if (theme === "dark") {
+    document.body.classList.add("dark-mode");
+}
+
+document.getElementById("toggleTheme").addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    localStorage.setItem(
+        "theme",
+        document.body.classList.contains("dark-mode") ? "dark" : "light"
+    );
+    atualizarIconeTema();
+});
+
+function atualizarIconeTema() {
+    const icone = document.getElementById("icone");
+    if (document.body.classList.contains("dark-mode")) {
+        icone.innerHTML = "<i class='bx bx-sun'></i>";
+    } else {
+        icone.innerHTML = "<i class='bx bx-moon'></i>";
+    }
+}
+
+atualizarIconeTema();
